27c706dbda7303a83082e9de5fee4660254a9c34
SojinPlusPark <sojinpark.seoul@gmail.com>
2023-09-28 16:30:10 +0200

    files

---
+++ src/Recipe.js
@@ -0,0 +1,21 @@
+function Recipe({ title, image, description }) {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img scr={image} alt="recipe" />
+                </figure>
+            </div>
+
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Recipe;
