5722d9cbef966b1e8f547c68c4ca18c5a1db77dc
SojinPlusPark <sojinpark.seoul@gmail.com>
2023-09-29 22:16:28 +0200

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,48 +1,51 @@
-import './Navbar.css'
+import "./Navbar.css"
+import logo_temp from "./images/logo_temp.png"
 
 function Navbar() {
-    const path = window.location.pathname
-    return <nav className="nav">
-        <a href="/home">Home</a>
-        <a href="/login">Login</a>
-    </nav>
+    return (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                    <img src="./images/logo_temp.png" alt="logo_zerowastegourmet" />
+                </a>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="Basic">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div className="navbar-menu" id="navbarBasic" >
+                <a className="navbar-item">
+                    Favorite
+                </a>
+
+                <a className="navbar-item">
+                    My Kitchen
+                </a>
+
+                <a className="navbar-item">
+                    Weekly Plan
+                </a>
+
+                <div className="navbar-item">
+                    <div className="buttons">
+                        <a className="button">
+                            Sign up
+                        </a>
+                        <a className="button">
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    );
 }
 
 export default Navbar;
 
-// <nav class="navbar" role="navigation" aria-label="main navigation">
 
-//     <div id="navbarBasicExample" class="navbar-menu">
-//         <div class="navbar-start">
-//             <a class="navbar-item">
-//                 Home
-//             </a>
-//         </div>
-//     </div>
-
-//     <div class="navbar-end">
-//         <div class="navbar-item">
-//             <div class="buttons">
-//                 <a class="button is-primary">
-//                     <strong>Sign up</strong>
-//                 </a>
-//                 <a class="button is-light">
-//                     Log in
-//                 </a>
-//             </div>
-//         </div>
-//     </div>
-
-// </nav>
-
-// function CustomLink({ href, ...props }) {
-//     return (
-//         <a href="/home">Home</a>
-//     <a href="/login">Login</a>
-
-//     )
-// }
-
-
-
-
+// is-fixed-top
