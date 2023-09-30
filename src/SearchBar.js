f48e9df36c1a3210d6b85d3cad707a0d022cd3be
SojinPlusPark <sojinpark.seoul@gmail.com>
2023-09-30 19:58:13 +0200

    Add files via upload

---
+++ src/SearchBar.js
@@ -0,0 +1,18 @@
+function SearchBar() {
+    return (
+        <div className="level-item" style={{ padding: "10rem" }}>
+            <div className="field has-addons">
+                <p className="control">
+                    <input class="input" type="text" placeholder="Type three ingredients" />
+                </p>
+                <p className="control">
+                    <button className="button">
+                        Find recipes
+                    </button>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar;
